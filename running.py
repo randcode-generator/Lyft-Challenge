75bd4db401f8b19de73bba1a2d3128160bb5671c
Eric Tang <tang8156@gmail.com>
2018-05-30 11:15:23 -0400

    .2/.96

--- running.py
+++ running.py
@@ -11,8 +11,8 @@ width_blocks = 5
 height_blocks = 2
 total_blocks = width_blocks * height_blocks
 epoch = 15
-car_prob_thres = 0.5
-road_prob_thres = 0.5
+car_prob_thres = 0.2
+road_prob_thres = 0.96
 
 file = sys.argv[-1]
 
